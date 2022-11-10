8129d4bcc5bec913a429931a8fad830f18a423fe
Eliyas Asefa <103815611+pinocchio-life-like@users.noreply.github.com>
2022-11-10 14:14:37 +0300

    Update Longest_Continuous_Subarray.py

--- Longest_Continuous_Subarray.py
+++ Longest_Continuous_Subarray.py
@@ -1,11 +1,10 @@
 class Solution:
     def longestSubarray(self, nums, limit): 
-        maxlen=1
+        lenmax=1
         for i in range(len(nums)):
             for j in range(i+1,len(nums)+1):
                 tmp=nums[i:j]
                 if max(tmp)-min(tmp)<=limit:
                     if len(tmp)>lenmax:
                         lenmax=len(tmp)
-        return maxlen
-
+        return lenmax
