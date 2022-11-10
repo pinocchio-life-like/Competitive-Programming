01436b3c94e0dfdad79086f97221856a6e5b15c2
Eliyas Asefa <103815611+pinocchio-life-like@users.noreply.github.com>
2022-11-10 08:33:07 +0300

    Update SortColor.py

--- SortColor.py
+++ SortColor.py
@@ -1,4 +1,4 @@
-def sortColors(nums):
+def sortColors(self, nums):
     """
     :type nums: List[int]
     :rtype: None Do not return anything, modify nums in-place instead.
@@ -10,4 +10,3 @@ def sortColors(nums):
                 
         
     return nums
-print(sortColors([2,0,2,1,1,0]))
